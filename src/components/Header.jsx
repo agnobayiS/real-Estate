363c0d8446d289ab96027ecf43612da762346d22
agnobayiS <C.ya@live.com>
2023-11-10 15:37:12 +0200

    pages and routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <span>siya</span>
+        <span>Estate</span>
+      </h1>
+    </header>
+  );
+}
